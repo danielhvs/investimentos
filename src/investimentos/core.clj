350c9d0f69cac1d0e97dae01e05ef4429cee74f0
Daniel Habib <daniel.habib@dataprev.gov.br>
2017-04-05 10:25:36 -0300

    Contas inciais com aporte.
    Falta corrigir e testar.

--- src/investimentos/core.clj
+++ src/investimentos/core.clj
@@ -3,15 +3,25 @@
   (:gen-class))
 
 (defn montante [dados]
-  (* (:capitalInicial dados) (math/expt  (+ 1 (/ (:aa dados) 100)) (:tAnos dados))))
+  (let [capitalInicial (* (:capitalInicial dados) (math/expt  (+ 1 (/ (:taxa dados) 100)) (:tempo dados)))]
+    {:capitalInicial capitalInicial :taxa (:taxa dados) :tempo (:tempo dados)}))
 
-(def teste 
-  {:aa 10.00 :capitalInicial 100000.00 :tAnos 26})
+(defn soma-montantes [m1 m2]
+  (assoc m2 :capitalInicial (+ (:capitalInicial m1) (:capitalInicial m2))))
 
-(comment def teste 
-  {:aa 10.00 :capitalInicial 0.00 :aporteMensal 250.00 :tMeses 24})
+(comment errado ainda. falta somar os montantes e fazer novo montante)
+(defn montante-com-aportes [dados n]
+  (let [m1 (soma-montantes (montante dados) dados)
+        m2 (soma-montantes (montante m1) m1)]
+    m2))
+
+(comment montante
+ (soma-montantes dados (montante dados)))
+
+(def aplicacao {:taxa 10.00 :capitalInicial 100000.00 :tempo 26})
+(def aporte {:taxa (/ 10.00 1) :capitalInicial 100.00 :tempo 1})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (montante teste)))
+  (println (montante-com-aportes aporte 3)))
