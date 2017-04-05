25f7c2805c3f77831caa14f72509483647ee508b
Daniel Habib <daniel.habib@dataprev.gov.br>
2017-04-05 09:28:26 -0300

    Comecando com formulas simples de juros compostos.

--- src/investimentos/core.clj
+++ src/investimentos/core.clj
@@ -1,11 +1,17 @@
 (ns investimentos.core
+  (:require [clojure.math.numeric-tower :as math])
   (:gen-class))
 
-(defn busca-melhor [arquivo]
-  (let [conteudo (slurp arquivo)]
-    (println conteudo)))
+(defn montante [dados]
+  (* (:capitalInicial dados) (math/expt  (+ 1 (/ (:aa dados) 100)) (:tAnos dados))))
+
+(def teste 
+  {:aa 10.00 :capitalInicial 100000.00 :tAnos 26})
+
+(comment def teste 
+  {:aa 10.00 :capitalInicial 0.00 :aporteMensal 250.00 :tMeses 24})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (busca-melhor (first args)))
+  (println (montante teste)))
