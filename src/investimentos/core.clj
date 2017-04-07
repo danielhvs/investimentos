78c94447e1fcf3ac289826c5c5afc8a1870585fd
Daniel Habib <daniel.habib@dataprev.gov.br>
2017-04-07 08:41:25 -0300

    adicionando comparacao com investimentos com/sem imposto de renda.

--- src/investimentos/core.clj
+++ src/investimentos/core.clj
@@ -1,10 +1,18 @@
 (ns investimentos.core
   (:require [clojure.math.numeric-tower :as math]
-            [clojure.pprint :refer :all])
+            [clojure.pprint :refer :all]
+            ;[clojurewerkz.money.format :refer :all]
+)
   (:gen-class))
 
+;(import java.util.Locale)
+;(defn reais [valor] (amount-of cu/BRL valor) (Locale. "pt" "BR"))
+
+(defn fator [dados]
+  (math/expt  (+ 1 (/ (:taxa dados) 100)) (:tempo dados)))
+
 (defn montante [dados]
-  (let [montante (* (:montante dados) (math/expt  (+ 1 (/ (:taxa dados) 100)) (:tempo dados)))]
+  (let [montante (* (:montante dados) (fator dados))]
     {:montante montante :taxa (:taxa dados) :tempo (:tempo dados)}))
 
 (defn inc-tempo [dados]
@@ -20,8 +28,35 @@
 (defn soma-montantes [seq-dados]
   (reduce + (map :montante seq-dados)))
 
+(defn ir [t]
+  (cond 
+   (<= t 180) 0.225
+   (<= t 360) 0.200
+   (<= t 540) 0.175
+   :else 0.150)
+)
+
+; tempo deve ser sempre em dias!
+(defn rendimento [dados]
+  (* (:montante dados) (- (fator dados) 1)))
+
+(defn rendimento-com-ir [dados]
+  (* (rendimento dados) (- 1 (ir (:tempo dados)))))
+
+(defn calcula-rendimento [dados]
+  (if (:ir dados) (rendimento-com-ir dados) (rendimento dados)))
+
+(defn calcula-melhor [seq-dados]
+  (let [rendimentos (map calcula-rendimento seq-dados)
+        maior-rendimento (apply max rendimentos)]
+    (filter #(= maior-rendimento (calcula-rendimento %)) seq-dados)
+    ))
+
+(def cdi 10.0)
 (def aplicacao {:taxa 1.00 :montante 282000.00 :tempo 1})
 (def aporte {:taxa 0.4167 :montante 500.00 :tempo 1})
+(def lca {:taxa (/ (* cdi (/ 80 100)) 365.0) :montante 100000.00 :tempo 361 :ir false :tipo "lca"})
+(def cdb {:taxa (/ (* cdi (/ 118 100)) 365.0) :montante 100000.00 :tempo 360 :ir true :tipo "cdb"})
 
 (defn -main
   [& args]
@@ -30,3 +65,7 @@
     (pprint seq-montantes)
     (pprint m)
     (pprint (str "rendimento mensal: " (- (:montante (montante {:taxa 0.4167 :montante m :tempo 1})) m)) )))
+
+(calcula-rendimento lca)
+(calcula-rendimento cdb)
+(calcula-melhor [lca cdb])
